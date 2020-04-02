bd4e759bde5aa6ad4eb7b6b14f8a4c32ac3b6b6b
olivierdesse <61228195+olivierdesse@users.noreply.github.com>
2020-04-02 09:19:07 +0200

    Update update_v1.sql

--- sql/update_v1.sql
+++ sql/update_v1.sql
@@ -2,7 +2,4 @@ ALTER TABLE tst.evenement
     ADD image MEDIUMBLOB;
 ALTER TABLE tst.evenement
     ADD horaire varchar(20) NOT NULL;
-ALTER TABLE tst.lien DROP id;
-ALTER TABLE tst.lien ADD PRIMARY KEY(idevent,idutilisateur);
-ALTER TABLE tst.utilisateurs DROP login;
-ALTER TABLE tst.utilisateurs ADD email VARCHAR(30);
+
